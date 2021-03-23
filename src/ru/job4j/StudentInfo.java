f7a21b7cffcd2e45445b2f46312952804b9eba88
Баландин Дмитрий Валерьевич <DBalandin@alfabank.ru>
2021-03-24 00:36:49 +0500

    3.3. Разбор первой программы. [#502544]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String [] args) {
+
+        System.out.println("Elena");
+        System.out.println("19.06.88");
+    }
+}
