84ce881e463f47380dac0ffb78899890ef0ad6c9
selen <selena-ek@yandex.ru>
2024-09-25 21:04:11 +0500

    4.1. Разбор первой программы. [#159033 #35667]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Elena Kurzanova");
+        System.out.println("19.06.1977");
+    }
+}
