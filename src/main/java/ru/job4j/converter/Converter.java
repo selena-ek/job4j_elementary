d11a08f44c35dc3b16c4d2a32587388f080353b2
selen <selena-ek@yandex.ru>
2024-12-07 23:21:39 +0500

    5. Конвертер валюты [#41583]

---
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -0,0 +1,21 @@
+package ru.job4j.converter;
+
+public class Converter {
+
+    public static float rubleToEuro(float value) {
+        float result = value / 70;
+        return result;
+    }
+
+    public static float rubleToDollar(float value) {
+        float result = value / 60;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        float euro = rubleToEuro(140);
+        float dollar = rubleToDollar(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubles are " + dollar + " dollar.");
+    }
+}
