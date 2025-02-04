cdf4c0a8f6a1ee689d4711932e1e77d25d8d2f15
selen <selena-ek@yandex.ru>
2025-02-04 15:05:24 +0500

    8. Стороны прямоугольника. [#137735 #530719]

--- src/main/java/ru/job4j/condition/RectangleArea.java
+++ src/main/java/ru/job4j/condition/RectangleArea.java
@@ -4,9 +4,7 @@ public class RectangleArea {
     public static double square(double p, double k) {
         double h = p / (2 * (k + 1));
         double l = h * k;
-        double s = l * h;
-        double result = s;
-        return result;
+        return l * h;
     }
 
     public static void main(String[] args) {
